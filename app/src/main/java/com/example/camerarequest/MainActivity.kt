1aaee1b8a8490e947d672c674b6b39ce86a50adc
sergey.startsev@x5.ru <56startcev5611RUS>
2021-11-11 22:44:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/camerarequest/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.camerarequest
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dispatchTakePictureIntent()
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            Log.e(this.javaClass.name, e.message ?: e.localizedMessage)
+        }
+    }
+}
